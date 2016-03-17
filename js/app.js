5136943ddb0a94275e32bc4541c2f94782829a29
Ari Quiñones Vargas <AriQuinones@Aris-MacBook-Pro.local>
2016-03-17 09:35:36 -0500

    tiyReactPractice

--- js/app.js
+++ js/app.js
@@ -27,6 +27,90 @@ function app() {
     // start app
     // new Router()
     DOM.render(<p>test 2</p>, document.querySelector('.container'))
+    var AppView = React.createClass({
+    	render: function() {
+    		return (
+    		<div className='headerContainer'>
+	    		<img className="headerImage" src="http://magentanova.github.io/html-intro-1/images/houston.jpg"/>
+	    		<div className="logoContainer">
+	    			<img className="tiyLogo" src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png"/>
+	    		</div>
+	    		<MainContainer/>
+	    		
+    		</div>
+    		)
+    	}
+    })
+    var MainContainer = React.createClass ({
+    	render: function () {
+    		return (
+    			<div className="mainContainer"> 
+	    			<SideColumnView/>
+		    		<MainColumnView/>
+		    		<SideTextBox/>
+    			</div>
+    			)
+    	}
+    })
+    var SideColumnView = React.createClass ({
+    	render: function() {
+    		return (
+    			<div className="sideColumnContainer">
+    				<h1> The Iron Yard | Houston </h1>
+    				<p> Happenings and updates from The Iron Yard in Houston, Tx</p>
+    				<div className="line"></div>
+    				<p> Search </p>
+    				<input type="text"/>
+    			</div>
+    			)
+    	}
+    })
+
+    var MainColumnView = React.createClass ({
+    	render: function() {
+    		return (
+    			<div className="mainColumnContainer">
+    				<h1> September 22 Starts a New Class of The Iron Yard Students</h1>
+    				<p>By Brian Dorton, Campus Director at The Iron Yard Houston</p>
+    				<div className="mainContainerImage">
+    					<img class="mainColumnImage" src="http://magentanova.github.io/html-intro-1/images/classroom.jpg"/>
+    				</div>
+    				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+    			)
+    	}
+    })
+    var SideTextBox = React.createClass({
+    	render: function() {
+    		return (
+	    		<div className="floatTextBoxContainer">
+	    			<h2>Never miss a post!</h2>
+	    			<div className="smallLogoContainer">
+	    				<img className="tiyLogo" src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png"/>
+	    			</div>
+	    			<div className="textBoxtextContainer">
+	    				<p>tiyHouston</p>
+	    				<p>The Iron Yard | Houston</p>
+	    			</div>
+	    			<button> + Follow </button>
+	    		</div>
+    			)
+    	}
+    })
+
+    DOM.render(<AppView/>,document.querySelector('.container'))
 }
 
 app()
